6f47540107a46e9239991d0a42f45fa02427c09b
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-10-03 23:59:06 +0100

    correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(0, 90):
     last_num = i % 10
     if (first_num < last_num):
         if (i != 89):
-            print("{:02d}".format(i), end=" ")
+            print("{:02d}, ".format(i), end=" ")
         else:
             print("{}".format(i))
