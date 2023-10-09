623f6b99c6693067a3fd2631ec3e69797f514c8c
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-10-10 00:27:39 +0100

    correction of task

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def aux_in_list(my_list, idx, element):
-    aux_list = my_list[]
-    if idx < 0 or idx >= len(my_list):
-        return (my_list)
 
-        my_list[idx] = element
-        return (aux_list)
+def aux_in_list(my_list, idx, element):
+    if my_list:
+        if idx < 0 or idx >= len(my_list):
+            return (my_list)
+        else:
+            my_list[idx] = element
+            return (aux_list)
